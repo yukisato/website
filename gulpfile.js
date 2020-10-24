eaca7e3a424ff43c9eb9e51370967c029a694b70
ayumitk <ayumee528@gmail.com>
2020-10-23 19:38:08 -0700

    modify: scriptをminify

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const ejs = require("gulp-ejs");
 const htmlmin = require("gulp-htmlmin");
 const header = require("gulp-header");
 const footer = require("gulp-footer");
+const uglify = require("gulp-uglify");
 const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
 const changed = require("gulp-changed");
@@ -34,6 +35,15 @@ const css = () =>
     .pipe(dest("./src/ejs"))
     .pipe(browserSync.stream());
 
+const js = () =>
+  src("./src/js/**/*.js")
+    .pipe(uglify())
+    .pipe(header("<script>"))
+    .pipe(footer("</script>"))
+    .pipe(rename("_script.ejs"))
+    .pipe(dest("./src/ejs"))
+    .pipe(browserSync.stream());
+
 const image = () =>
   src("src/images/**/*.+(jpg|jpeg|png|gif)")
     .pipe(changed("./docs/images"))
@@ -58,11 +68,13 @@ const watchFiles = () =>
   });
 watch("./src/ejs/**/*.ejs", html);
 watch("./src/scss/**/*.scss", css);
+watch("./src/js/**/*.js", js);
 watch("./src/images/**/*", image);
 watch("./docs/**/*").on("change", browserSync.reload);
 
 exports.html = html;
 exports.css = css;
+exports.js = js;
 exports.image = image;
 exports.watchFiles = watchFiles;
-exports.default = parallel(css, html, image, watchFiles);
+exports.default = parallel(css, js, html, image, watchFiles);
